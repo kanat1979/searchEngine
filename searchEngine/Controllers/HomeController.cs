11fc2d6ec3465a367ca395d8541f2780ff9d2ead
kanat <kanat@rambler.ru>
2016-12-22 00:34:01 +0600

    first commit

---
+++ searchEngine/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Google.Apis.Customsearch.v1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace searchEngine.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    [HttpPost]
+        public ActionResult GoogleSearch()
+        {
+            const string apiKey = "Your ApiKey";
+            const string searchEngineId = "003470263288780838160:ty47piyybua";
+            string query = Request.Form["query"];
+            CustomsearchService customSearchService = new CustomsearchService(new Google.Apis.Services.BaseClientService.Initializer() { ApiKey = apiKey });
+            Google.Apis.Customsearch.v1.CseResource.ListRequest listRequest = customSearchService.Cse.List(query);
+            listRequest.Cx = searchEngineId;
+            var search = listRequest.Execute();
+            ViewBag.Message = search;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+       /* [HttpPost]
+        public ActionResult Index()
+        {
+            ViewBag.Title = "test";
+            return RedirectToAction("index");
+            
+        }*/
+    }
+}
---
+++ searchEngine/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="searchEngine.MvcApplication" Language="C#" %>
